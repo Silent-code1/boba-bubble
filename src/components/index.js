b1d2115468a460381764625e927a091a4635f131
Silent-code1 <64768412+Silent-code1@users.noreply.github.com>
2023-04-03 11:41:54 +0200

    index components

---
+++ src/components/index.js
@@ -0,0 +1,20 @@
+import NavBar from './NavBar';
+import Bubble from './Bubble';
+import JuiceSirup from './JuiceSirup';
+import MainAbout from './MainAbout';
+import TeeChoose from './TeeChoose';
+import ReadyGo from './ReadyGo';
+import OurContacts from './OurContacts';
+import Footer from './Footer';
+
+
+export {
+  Bubble,
+  MainAbout,
+  JuiceSirup,
+  ReadyGo,
+  TeeChoose,
+  Footer,
+  NavBar,
+  OurContacts,
+};
