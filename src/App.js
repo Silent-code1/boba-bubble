9cf34614fedd868f590c4dc339b0c8a888526684
Silent-code1 <64768412+Silent-code1@users.noreply.github.com>
2023-04-03 11:34:42 +0200

    main  app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import styles from './style';
+import {
+  Bubble,
+  MainAbout,
+  JuiceSirup,
+  ReadyGo,
+  TeeChoose,
+  Footer,
+  NavBar,
+  OurContacts,
+} from './components';
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+  <div className='bg-primary w-full overflow-hidden'>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <NavBar />
+      </div>
+    </div>
 
-export default App
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <MainAbout />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <TeeChoose />
+        <Bubble />
+        <JuiceSirup />
+        <ReadyGo />
+        <OurContacts />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
