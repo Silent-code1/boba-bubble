161392ba6e66e2e631273cddeda3ef3e53665a20
Silent-code1 <64768412+Silent-code1@users.noreply.github.com>
2023-04-10 22:59:49 +0200

    style main

--- src/components/IngredCard.jsx
+++ src/components/IngredCard.jsx
@@ -2,11 +2,11 @@
 const IngredCard = ({ name, title, img }) => (
   <div className='flex justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5'>
     <div className='flex flex-col ml-4'>
-      <img src={img} alt={name} className='w-[108px] h-[108px] rounded-full mb-5' />
-      <h4 className='font-poppins font-semibold text-[20px] leading-[32px] text-white'>
+      <img src={img} alt={name} className='w-[158px] h-[158px] rounded-full mb-5' />
+      <h4 className='font-poppins font-semibold text-[22px] leading-[32px] text-white'>
         {name}
       </h4>
-      <p className='font-poppins font-normal text-[16px] leading-[24px] text-dimWhite'>
+      <p className='font-poppins font-normal text-[18px] leading-[24px] text-dimWhite'>
         {title}
       </p>
     </div>
--- src/components/MainAbout.jsx
+++ src/components/MainAbout.jsx
@@ -13,17 +13,18 @@ const MainAbout = () => {
         <div className='flex justify-between items-center w-full'>
           <h1 className='flex-1 font-poppins font-semibold ss:text-[52px] text-[32px] text-white ss:leading-[100.8px] leading-[75px]'>
             Call it
-            <span className='text-gradient'> Bubble tea</span>, milk tea or{' '}
-            <br className='sm:block hidden' />{' '}
-            <span className='text-gradient'>Boba</span> — it’s delicious!
+            <span className='text-gradient'> Bubble tea</span>, milk tea or
+            <span className='text-gradient'> Boba</span> — it’s delicious!
           </h1>
         </div>
 
-        <p className={`${styles.paragraph} max-w-[870px] mt-5`}>
+        <p className={`${styles.paragraph} max-w-[1200px] mt-5`}>
           Call it milk tea, boba, bubble tea or pearl milk tea, It’s got many
           names. They all mean that delicious, sweet cold drink that usually
           comes with different toppings. From the chewy boba pearls to the
-          scrumptious red bean.
+          scrumptious red bean. Call it milk tea, boba, bubble tea or pearl milk
+          tea, It’s got many names. They all mean that delicious, sweet cold
+          drink that usually comes with different toppings.
         </p>
       </div>
       <div className='flex-1 flex justify-between items-center'>
